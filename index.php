6f84d53051b2c7c3082684f79f2ce3f8de303803
Uwe <Uwe.GitRepos@topmail-files.de>
2017-09-16 18:50:18 +0200

    Index page changed

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "<h1>Hallo there to my Webseite</h1>";
-    echo "<br/ >";
-    echo "This is a test website";
-    echo "<br/ >";
-    echo "< a href = \"ReadMe.html\" target = \"_blank\">ReadMe</a>";
-?>
